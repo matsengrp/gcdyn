2e3280bf9a4f2c57615367ab7d3cff005fceefe9
Alyssa Pratt <alyssa@zpratts.net>
2022-06-22 15:32:03 -0700

    reformat fitness and replay, fix undefineds

--- gcdyn/fitness.py
+++ gcdyn/fitness.py
@@ -1,5 +1,7 @@
 r"""Uses phenotype to determine fitness"""
 import replay
+from math import exp
+
 
 class Fitness:
     r"""Class to determine fitness from phenotype for sequences
@@ -10,73 +12,108 @@ class Fitness:
     """
 
     def __init__(self, fasta_path: str = None, DNA_seq_list: list[str] = None):
-        if fasta_path != None:
-            seqs_df = replay.seq_df_tdms(replay.igh_frame, replay.igk_frame, replay.igk_idx, fasta_path)
-        elif DNA_seq_list != None:
-            seqs_df = replay.seq_df_tdms(replay.igh_frame, replay.igk_frame, replay.igk_idx, DNA_seq_list)
+        if fasta_path is not None:
+            seqs_df = replay.seq_df_tdms(
+                replay.igh_frame, replay.igk_frame, replay.igk_idx, fasta_path
+            )
+        elif DNA_seq_list is not None:
+            seqs_df = replay.seq_df_tdms(
+                replay.igh_frame, replay.igk_frame, replay.igk_idx, DNA_seq_list
+            )
         else:
-            raise Exception("Must define a path to DNA sequences or a list of sequences")
-        delta_log_KDs = replay.evaluate(replay.model, self.seqs_df["aa_sequence"], replay.tdms_phenotypes)
-        self.all_info_df = seqs_df.merge(delta_log_KDs, left_index=True, right_index=True)
+            raise Exception(
+                "Must define a path to DNA sequences or a list of sequences"
+            )
+        delta_log_KDs = replay.evaluate(
+            replay.model, self.seqs_df["aa_sequence"], replay.tdms_phenotypes
+        )
+        self.all_info_df = seqs_df.merge(
+            delta_log_KDs, left_index=True, right_index=True
+        )
 
-
-    def frac_antigen_bound(self, delta_log_KD, log10_naive_KD: float, concentration_antigen: float):
+    def frac_antigen_bound(
+        self, delta_log_KD, log10_naive_KD: float, concentration_antigen: float
+    ):
         """"""
         delta_log_KDs = self.all_info_df["delta_log10_KD"]
         thetas = []
-        for delta_log_KD in KDs:
-            log10_KD  = delta_log_KD + log10_naive_KD
-            KD = 10 ** log10_KD
+        for delta_log_KD in delta_log_KDs:
+            log10_KD = delta_log_KD + log10_naive_KD
+            KD = 10**log10_KD
             # Hill equation with n = 1:
-            theta = concentration_antigen/(KD + concentration_antigen)
+            theta = concentration_antigen / (KD + concentration_antigen)
             thetas.append(theta)
         self.all_info_df["frac_antigen_bound"] = thetas
 
-
-    def linear_fitness(self, slope: float, log10_naive_KD: float, concentration_antigen: float):
+    def linear_fitness(
+        self, slope: float, log10_naive_KD: float, concentration_antigen: float
+    ):
         """Combines methods to get the antigen bound, Tfh help, and fitness from the KD using a linear model"""
         for idx, row in self.all_info_df.iterrows():
-            antigen_bound = self.frac_antigen_bound(row.delta_log_KD, log10_naive_KD, concentration_antigen)
+            antigen_bound = self.frac_antigen_bound(
+                row.delta_log_KD, log10_naive_KD, concentration_antigen
+            )
             Tfh_help = antigen_bound * slope
-            all_info_df.loc[idx, "fitness"] = Tfh_help
-        return all_info_df['fitness']
+            self.all_info_df.loc[idx, "fitness"] = Tfh_help
+        return self.all_info_df["fitness"]
 
-    def sigmoidal_fitness(self, maximum_Tfh: float, curve_steepness: float,
-        midpoint_antigen_bound: float, log10_naive_KD: float,
-        concentration_antigen: float):
+    def sigmoidal_fitness(
+        self,
+        maximum_Tfh: float,
+        curve_steepness: float,
+        midpoint_antigen_bound: float,
+        log10_naive_KD: float,
+        concentration_antigen: float,
+    ):
         """Combines methods to get the antigen bound, Tfh help, and fitness from the KD using a sigmoidal model"""
         for idx, row in self.all_info_df.iterrows():
-            antigen_bound = self.frac_antigen_bound(row.delta_log_KD, log10_naive_KD, concentration_antigen)
-            Tfh_help = maximum_Tfh / (1 + exp(-1 * curve_steepness * (antigen_bound - midpoint_antigen_bound)))
-            fitness_df.loc[idx, "fitness"] = Tfh_help
-        return all_info_df['fitness']
+            antigen_bound = self.frac_antigen_bound(
+                row.delta_log_KD, log10_naive_KD, concentration_antigen
+            )
+            Tfh_help = maximum_Tfh / (
+                1 + exp(-1 * curve_steepness * (antigen_bound - midpoint_antigen_bound))
+            )
+            self.all_info_df.loc[idx, "fitness"] = Tfh_help
+        return self.all_info_df["fitness"]
 
-
-    def fitness(self, mapping_type: str = 'linear',
-        log10_naive_KD: float = -10.43, concentration_antigen: float = 10**(-9),
-        slope: float = 1, maximum_Tfh: float = 4, curve_steepness: float = 10,
-        midpoint_antigen_bound: float = 0.5):
+    def fitness(
+        self,
+        mapping_type: str = "linear",
+        log10_naive_KD: float = -10.43,
+        concentration_antigen: float = 10 ** (-9),
+        slope: float = 1,
+        maximum_Tfh: float = 4,
+        curve_steepness: float = 10,
+        midpoint_antigen_bound: float = 0.5,
+    ):
         r"""Maps evaluation to unnormalized fitness
         Args:
             mapping_type: type of mapping function (defaults to linear)
         """
-        if mapping_type == 'linear':
+        if mapping_type == "linear":
             return self.linear_fitness(slope, log10_naive_KD, concentration_antigen)
-        elif mapping_type == 'sigmoid':
-            return self.sigmoidal_fitness(maximum_Tfh, curve_steepness,
-                        midpoint_antigen_bound, log10_naive_KD, concentration_antigen)
+        elif mapping_type == "sigmoid":
+            return self.sigmoidal_fitness(
+                maximum_Tfh,
+                curve_steepness,
+                midpoint_antigen_bound,
+                log10_naive_KD,
+                concentration_antigen,
+            )
         else:
-            raise Exception('Only linear and sigmoid are acceptable mapping types')
-
+            raise Exception("Only linear and sigmoid are acceptable mapping types")
 
     def normalize_fitness(self):
         """Normalize fitness from a dataframe with a fitness column"""
-        sum_fitness = self.all_info_df['fitness'].sum()
-        self.all_info_df['normalized_fitness'] = (self.all_info_df['fitness']) / (sum_fitness)
-        return self.all_info_df['normalized_fitness']
-
+        sum_fitness = self.all_info_df["fitness"].sum()
+        self.all_info_df["normalized_fitness"] = (self.all_info_df["fitness"]) / (
+            sum_fitness
+        )
+        return self.all_info_df["normalized_fitness"]
 
     def map_cell_divisions(self, slope: float = 1):
         """Map fitness linearly to the number of cell divisions using slope"""
-        self.all_info_df['cell_divisions'] = self.all_info_df['normalized_fitness'] * slope
-        return self.all_info_df['cell_divisions']
+        self.all_info_df["cell_divisions"] = (
+            self.all_info_df["normalized_fitness"] * slope
+        )
+        return self.all_info_df["cell_divisions"]
--- gcdyn/replay.py
+++ gcdyn/replay.py
@@ -1,56 +1,62 @@
-import pandas as pd, torch, numpy
+import pandas as pd
+import torch
 from Bio import SeqIO
 from Bio.Seq import Seq
-from plotnine import ggplot, geom_histogram, aes, facet_wrap, ggtitle, xlim, ylim, geom_point
-from math import exp
-
 igh_frame = 1
 igk_frame = 1
 igk_idx = 336
 naive_sites_path = "https://raw.githubusercontent.com/jbloomlab/Ab-CGGnaive_DMS/main/data/CGGnaive_sites.csv"
 model_path = "../notebooks/Linear.model"
 model = torch.load(model_path)
-tdms_phenotypes = ['delta_log10_KD', 'expression']
+tdms_phenotypes = ["delta_log10_KD", "expression"]
 
 
 # From gcreplay-tools (https://github.com/matsengrp/gcreplay/blob/main/nextflow/bin/gcreplay-tools.py):
+
 
 def fasta_to_df(f):
     """simply convert a fasta to dataframe"""
     ids, seqs = [], []
     with open(f) as fasta_file:
-        for seq_record in SeqIO.parse(fasta_file, 'fasta'):  # (generator)
-            if str(seq_record.id) != 'naive':
+        for seq_record in SeqIO.parse(fasta_file, "fasta"):  # (generator)
+            if str(seq_record.id) != "naive":
                 ids.append(seq_record.id)
                 seqs.append(str(seq_record.seq))
-    return pd.DataFrame({"id":ids, "seq":seqs})
+    return pd.DataFrame({"id": ids, "seq": seqs})
 
 
 def aa(sequence, frame):
     """Amino acid translation of nucleotide sequence in frame 1, 2, or 3."""
     return Seq(
-        sequence[(frame - 1): (frame - 1
-                               + (3 * ((len(sequence) - (frame - 1)) // 3)))]
+        sequence[(frame - 1) : (frame - 1 + (3 * ((len(sequence) - (frame - 1)) // 3)))]
     ).translate()
+
 
 def read_sites_file(naive_sites_path: str):
     """Read the sites file from csv"""
     # collect the correct sites df from tylers repo
     pos_df = pd.read_csv(
-            naive_sites_path,
-            dtype=dict(site=pd.Int16Dtype()),
-            index_col="site_scFv",
-        )
+        naive_sites_path,
+        dtype=dict(site=pd.Int16Dtype()),
+        index_col="site_scFv",
+    )
     return pos_df
 
-def seq_df_tdms(igh_frame: int, igk_frame: int, igk_idx: int, fasta_path: str = None, seq_list: list[str] = None):
+
+def seq_df_tdms(
+    igh_frame: int,
+    igk_frame: int,
+    igk_idx: int,
+    fasta_path: str = None,
+    seq_list: list[str] = None,
+):
     """Make amino acid sequence predictions using chain information and return as a dataframe"""
 
-    if fasta_path != None:
+    if fasta_path is not None:
         # load the seqs from a fasta
         seqs_df = fasta_to_df(fasta_path)
-    elif seq_list != None:
-        seqs_df = pd.DataFrame({"seq":seqs})
+    elif seq_list is not None:
+        seqs_df = pd.DataFrame({"seq": seq_list})
     else:
         raise Exception("fasta path or list of sequences must be given")
 
@@ -75,11 +81,14 @@ def seq_df_tdms(igh_frame: int, igk_frame: int, igk_idx: int, fasta_path: str = 
 
 # phenotype evaluation:
 
+
 def evaluate(torchdms_model, seqs: list[str], phenotype_names: list[str]):
     """Evaluate sequences using torchdms model and return the evaluation as a pandas dataframe"""
     aa_seq_one_hot = torch.stack([torchdms_model.seq_to_binary(seq) for seq in seqs])
     try:
-        labeled_evaluation = pd.DataFrame(torchdms_model(aa_seq_one_hot).detach().numpy(), columns=phenotype_names)
+        labeled_evaluation = pd.DataFrame(
+            torchdms_model(aa_seq_one_hot).detach().numpy(), columns=phenotype_names
+        )
     except ValueError:
         print("Incorrect number of column labels for phenotype data")
     return labeled_evaluation
