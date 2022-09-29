97678c8ae16e3b982be8b53fb3f03aed806cb1e8
WSDeWitt <wsdewitt@gmail.com>
2022-09-29 16:17:15 -0700

    tidying docs build

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ autodoc_default_options = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ["templates"]
 
 # -- Options for HTML output -------------------------------------------------
 
