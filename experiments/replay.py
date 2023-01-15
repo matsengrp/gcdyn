545ef222b9c1cfb7ceae33b5f8e6c69acd4fdd54
Erick Matsen <ematsen@gmail.com>
2023-01-15 14:10:02 -0800

    move more to replay module

--- experiments/replay.py
+++ experiments/replay.py
@@ -1,8 +1,41 @@
 """
 Various things relevant for the replay experiment.
 """
+import os
+import sys
 
+import pandas as pd
 
-# TODO Will can you double check this?
+from gcdyn import bdms, gpmap, mutators, responses, utils
+
+# TODO Will can you double check this naive sequence?
 
 naive_sequence = "GAGGTGCAGCTTCAGGAGTCAGGACCTAGCCTCGTGAAACCTTCTCAGACTCTGTCCCTCACCTGTTCTGTCACTGGCGACTCCATCACCAGTGGTTACTGGAACTGGATCCGGAAATTCCCAGGGAATAAACTTGAGTACATGGGGTACATAAGCTACAGTGGTAGCACTTACTACAATCCATCTCTCAAAAGTCGAATCTCCATCACTCGAGACACATCCAAGAACCAGTACTACCTGCAGTTGAATTCTGTGACTACTGAGGACACAGCCACATATTACTGTGCAAGGGACTTCGATGTCTGGGGCGCAGGGACCACGGTCACCGTCTCCTCAGACATTGTGATGACTCAGTCTCAAAAATTCATGTCCACATCAGTAGGAGACAGGGTCAGCGTCACCTGCAAGGCCAGTCAGAATGTGGGTACTAATGTAGCCTGGTATCAACAGAAACCAGGGCAATCTCCTAAAGCACTGATTTACTCGGCATCCTACAGGTACAGTGGAGTCCCTGATCGCTTCACAGGCAGTGGATCTGGGACAGATTTCACTCTCACCATCAGCAATGTGCAGTCTGAAGACTTGGCAGAGTATTTCTGTCAGCAATATAACAGCTATCCTCTCACGTTCGGCTCGGGGACTAAGCTAGAAATAAAA"
+
+chain_2_start_idx = 336
+
+gcdyn_data_dir = os.path.join(os.path.dirname(sys.modules["gcdyn"].__file__), "data")
+
+
+mutability = pd.read_csv(
+    os.path.join(gcdyn_data_dir, "MK_RS5NF_mutability.csv"), index_col=0
+).squeeze("columns")
+
+substitution = pd.read_csv(
+    os.path.join(gcdyn_data_dir, "MK_RS5NF_substitution.csv"), index_col=0
+)
+
+
+def seq_to_contexts(seq):
+    return utils.padded_fivemer_contexts_of_paired_sequences(seq, chain_2_start_idx)
+
+
+mutator = mutators.ContextMutator(
+    mutability=mutability,
+    substitution=substitution,
+    seq_to_contexts=seq_to_contexts,
+)
+
+mutation_response = responses.SequenceContextMutationResponse(
+    mutability, seq_to_contexts
+)
--- tests/test_sequence_mutator.py
+++ tests/test_sequence_mutator.py
@@ -13,38 +13,6 @@ def uniform_mutator():
 
 
 @pytest.fixture
-def mutability(MK_RS5NF_mutability_path):
-    return pd.read_csv(MK_RS5NF_mutability_path, index_col=0).squeeze("columns")
-
-
-@pytest.fixture
-def substitution(MK_RS5NF_substitution_path):
-    return pd.read_csv(MK_RS5NF_substitution_path, index_col=0)
-
-
-@pytest.fixture
-def replay_seq_to_contexts():
-    chain_2_start_idx = 336
-    return lambda seq: utils.padded_fivemer_contexts_of_paired_sequences(
-        seq, chain_2_start_idx
-    )
-
-
-@pytest.fixture
-def replay_mutator(mutability, substitution, replay_seq_to_contexts):
-    return mutators.ContextMutator(
-        mutability=mutability,
-        substitution=substitution,
-        seq_to_contexts=replay_seq_to_contexts,
-    )
-
-
-@pytest.fixture
-def replay_mutation_response(mutability, substitution, replay_seq_to_contexts):
-    return responses.SequenceContextMutationResponse(mutability, replay_seq_to_contexts)
-
-
-@pytest.fixture
 def gp_map():
     return gpmap.ConstantGPMap(1.0)
 
@@ -73,13 +41,6 @@ def test_uniform_mutator(uniform_mutator, gp_map):
     mutator.mutate(node)
 
 
-def test_fivemer_mutator(replay_mutator, replay_naive):
-    node = bdms.TreeNode()
-    node.sequence = replay_naive
-    for i in range(10):
-        replay_mutator.mutate(node)
-
-
 def test_mutator_in_tree(uniform_mutator, gp_map):
     tree = bdms.TreeNode()
     tree.sequence = "AGCT"
@@ -96,23 +57,3 @@ def test_mutator_in_tree(uniform_mutator, gp_map):
         except bdms.TreeError:
             continue
     print([node.sequence for node in tree.iter_leaves()])
-
-
-def test_fivemer_mutator_in_tree(
-    replay_mutator, gp_map, replay_naive, replay_mutation_response
-):
-    tree = bdms.TreeNode()
-    tree.sequence = replay_naive
-    tree.x = gp_map(tree.sequence)
-    for seed in range(1000):
-        try:
-            tree.evolve(
-                5,
-                mutator=mutators.SequencePhenotypeMutator(replay_mutator, gp_map),
-                mutation_rate=replay_mutation_response,
-                min_survivors=20,
-                seed=seed,
-            )
-            break
-        except bdms.TreeError:
-            continue
