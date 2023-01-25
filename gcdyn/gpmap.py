ddee6c659d4b174aed55f5a9b61f46f9ddcab236
Erick Matsen <ematsen@gmail.com>
2023-01-25 14:34:44 -0800

    Update gcdyn/gpmap.py
    Co-authored-by: William DeWitt <wsdewitt@gmail.com>

--- gcdyn/gpmap.py
+++ gcdyn/gpmap.py
@@ -26,7 +26,7 @@ class GPMap(ABC):
         pass
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({', '.join(f'{key}={value}' for key, value in self.__dict__.items())})"
+        return f"{self.__class__.__name__}({', '.join(f'{key}={value}' for key, value in vars(self).items() if not key.startswith('_'))})"
 
 
 class ConstantGPMap(GPMap):
