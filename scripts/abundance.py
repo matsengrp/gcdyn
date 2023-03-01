1a62d233edcc35df0f428fb6ea4c72a33847814b
Erick Matsen <ematsen@gmail.com>
2023-03-01 05:28:30 -0800

    abundance and multi-simulation

---
+++ scripts/abundance.py
@@ -0,0 +1,41 @@
+"""
+Calculate the sequence abundance frequency for each input fasta and then write it as a CSV.
+"""
+
+import collections
+import os
+import sys
+import pandas as pd
+
+from Bio import SeqIO
+from Bio.SeqUtils.CheckSum import seguid
+
+abundances = {}
+
+for fasta_path in sys.argv[1:]:
+
+    records = SeqIO.parse(fasta_path, "fasta")
+
+    # This dictionary will map sequence checksum to the list of squence ids that have that
+    # sequence checksum.
+    ids_by_checksum = collections.defaultdict(list)
+    sequence_count = 0
+    for record in records:
+        sequence_count = sequence_count + 1
+        ids_by_checksum[seguid(record.seq)].append(record.id)
+
+    abundance_distribution = collections.defaultdict(int)
+
+    for id_list in ids_by_checksum.values():
+        id_count = len(id_list)
+        abundance_distribution[id_count] = abundance_distribution[id_count] + 1
+
+    assert sequence_count == sum(k * v for k, v in abundance_distribution.items())
+
+    base, _ = os.path.splitext(fasta_path)
+    abundances[base] = pd.Series(
+        abundance_distribution.values(), index=abundance_distribution.keys()
+    )
+
+to_write = pd.DataFrame(abundances).fillna(0).astype(int)
+to_write.to_csv("abundances.csv")
---
+++ scripts/multi-simulation.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from gcdyn import bdms, gpmap, mutators, responses, utils
+from experiments import replay
+
+
+def generate_sequences_and_tree(
+    birth_rate, death_rate, mutation_rate, mutator, seed=0, time_to_sampling=20
+):
+
+    for iter in range(1000):
+        try:
+            tree = bdms.TreeNode()
+            tree.sequence = replay.NAIVE_SEQUENCE
+            tree.x = gp_map(tree.sequence)
+            tree.evolve(
+                time_to_sampling,
+                birth_rate=birth_rate,
+                death_rate=death_rate,
+                mutation_rate=mutation_rate,
+                mutator=mutator,
+                min_survivors=100,
+                max_leaves=1000,
+                birth_mutations=False,
+                seed=seed,
+            )
+            print(f"try {iter + 1} succeeded, tip count: {len(tree)}")
+            break
+        except bdms.TreeError as e:
+            print(f"try {iter + 1} failed, {e}", flush=True)
+            continue
+
+    tree.sample_survivors(n=60, seed=0)
+    tree.prune()
+
+    return tree
+
+
+def average_mutations(tree):
+    tree.total_mutations = 0
+
+    for node in tree.iter_descendants(strategy="preorder"):
+        node.total_mutations = node.n_mutations + node.up.total_mutations
+
+    return np.mean([leaf.total_mutations for leaf in tree.iter_leaves()])
+
+
+gp_map = gpmap.AdditiveGPMap(
+    replay.bind_df, nonsense_phenotype=replay.bind_df.min().min()
+)
+assert gp_map(replay.NAIVE_SEQUENCE) == 0
+
+birth_rate = responses.SoftReluResponse(xscale=2, xshift=-2.5, yscale=0.05, yshift=0)
+death_rate = responses.ConstantResponse(0.025)
+
+mutability_multiplier = 1
+mutator = mutators.SequencePhenotypeMutator(
+    mutators.ContextMutator(
+        mutability=mutability_multiplier * replay.mutability,
+        substitution=replay.substitution,
+        seq_to_contexts=replay.seq_to_contexts,
+    ),
+    gp_map,
+)
+
+mutation_rate = responses.SequenceContextMutationResponse(
+    mutability_multiplier * replay.mutability, replay.seq_to_contexts
+)
+
+for i in range(1, 11):
+    print("trial #", i)
+    rng = np.random.default_rng(seed=i * i)
+    tree = generate_sequences_and_tree(
+        birth_rate, death_rate, mutation_rate, mutator, seed=rng
+    )
+
+    print("average # of mutations per sequence", average_mutations(tree))
+
+    with open(f"tree_{i}.nwk", "w") as fp:
+        fp.write(tree.write() + "\n")
+
+    utils.write_leaf_sequences_to_fasta(
+        tree,
+        f"seqs_{i}.fasta",
+        naive=replay.NAIVE_SEQUENCE,
+    )
