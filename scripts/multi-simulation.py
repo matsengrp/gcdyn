9ce19c351e6d41ba1a3d98f20cda7ddd65b5712f
duncan ralph <dkralph@gmail.com>
2023-03-03 14:31:54 -0800

    add some args to multi run script

--- scripts/multi-simulation.py
+++ scripts/multi-simulation.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+import os
 
 from gcdyn import bdms, gpmap, mutators, responses, utils
 from experiments import replay
 
-
+# ----------------------------------------------------------------------------------------
 def generate_sequences_and_tree(
     birth_rate, death_rate, mutation_rate, mutator, seed=0, time_to_sampling=20
 ):
@@ -31,12 +33,12 @@ def generate_sequences_and_tree(
             print(f"try {iter + 1} failed, {e}", flush=True)
             continue
 
-    tree.sample_survivors(n=60, seed=0)
+    tree.sample_survivors(n=args.n_seqs, seed=0)
     tree.prune()
 
     return tree
 
-
+# ----------------------------------------------------------------------------------------
 def average_mutations(tree):
     tree.total_mutations = 0
 
@@ -45,6 +47,15 @@ def average_mutations(tree):
 
     return np.mean([leaf.total_mutations for leaf in tree.iter_leaves()])
 
+# ----------------------------------------------------------------------------------------
+parser = argparse.ArgumentParser()
+parser.add_argument('--n-seqs', default=60, type=int, help='Number of sequences to observe')
+parser.add_argument('--n-trials', default=10, type=int, help='Number of trials/GCs to simulate')
+parser.add_argument('--outdir', default=os.getcwd())
+args = parser.parse_args()
+
+if not os.path.exists(args.outdir):
+    os.makedirs(args.outdir)
 
 gp_map = gpmap.AdditiveGPMap(
     replay.bind_df, nonsense_phenotype=replay.bind_df.min().min()
@@ -68,7 +79,7 @@ mutation_rate = responses.SequenceContextMutationResponse(
     mutability_multiplier * replay.mutability, replay.seq_to_contexts
 )
 
-for i in range(1, 11):
+for i in range(1, args.n_trials + 1):
     print("trial #", i)
     rng = np.random.default_rng(seed=i * i)
     tree = generate_sequences_and_tree(
@@ -77,11 +88,11 @@ for i in range(1, 11):
 
     print("average # of mutations per sequence", average_mutations(tree))
 
-    with open(f"tree_{i}.nwk", "w") as fp:
+    with open(f"{args.outdir}/tree_{i}.nwk", "w") as fp:
         fp.write(tree.write() + "\n")
 
     utils.write_leaf_sequences_to_fasta(
         tree,
-        f"seqs_{i}.fasta",
+        f"{args.outdir}/seqs_{i}.fasta",
         naive=replay.NAIVE_SEQUENCE,
     )
