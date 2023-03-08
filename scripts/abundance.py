26443f743ebd80fa1e13573fb081fa3772dc7550
duncan ralph <dkralph@gmail.com>
2023-03-07 16:03:56 -0800

    add root tip distance

--- scripts/abundance.py
+++ scripts/abundance.py
@@ -12,6 +12,12 @@ import numpy
 from Bio import SeqIO
 from Bio.SeqUtils.CheckSum import seguid
 
+# ----------------------------------------------------------------------------------------
+def hamming_distance(seq1, seq2):  # NOTE doesn't handle ambiguous bases
+    assert len(seq1) == len(seq2)
+    return sum(x != y for x, y in zip(seq1.upper(), seq2.upper()))
+
+# ----------------------------------------------------------------------------------------
 ustr = """
 python abundance.py <input fasta files> [--outdir <outdir>]
 """
@@ -20,13 +26,29 @@ parser.add_argument('infiles', nargs='+')
 parser.add_argument('--outdir', default=os.getcwd())
 parser.add_argument('--min-seqs', type=int, help='only keep GCs that have this many seqs')
 parser.add_argument('--max-seqs', type=int, help='downsample to this many seqs any GCs that have more than this')
+parser.add_argument('--naive-name', default='naive', help='name of naive sequence, so that it can be skipped')
+parser.add_argument('--random-seed', type=int, default=1, help='random seed for subsampling')
 args = parser.parse_args()
 
-n_too_small, init_sizes = 0, []
-abundances = {}
+numpy.random.seed(args.random_seed)
+
+n_too_small, init_sizes, n_removed = 0, [], 0
+final_distrs = {k : {} for k in ['abundances', 'hdists']}  # hdist: hamming distance from naive (root-to-tip distance)
 
 for fasta_path in args.infiles:
     records = list(SeqIO.parse(fasta_path, "fasta"))
+    naive_record = None
+    if args.naive_name is not None:
+        n_before = len(records)
+        new_records = []
+        for rcd in records:
+            if rcd.id == args.naive_name:  # doesn't check whether there's more than one naive sequence
+                naive_record = rcd
+            else:
+                new_records.append(rcd)
+        records = new_records
+        if len(records) != n_before:
+            n_removed += n_before - len(records)
     if args.min_seqs is not None and len(records) < args.min_seqs:
         n_too_small += 1
         continue
@@ -37,34 +59,44 @@ for fasta_path in args.infiles:
 
     # This dictionary will map sequence checksum to the list of squence ids that have that
     # sequence checksum.
-    ids_by_checksum = collections.defaultdict(list)
+    ids_by_checksum = {k : collections.defaultdict(list) for k in final_distrs}
     sequence_count = 0
     for record in records:
         sequence_count = sequence_count + 1
-        ids_by_checksum[seguid(record.seq)].append(record.id)
+        for tkey in final_distrs:
+            if tkey == 'abundances':
+                vkey = seguid(record.seq)
+            elif tkey == 'hdists':
+                vkey = hamming_distance(record.seq, naive_record.seq)
+            else:
+                assert False
+            ids_by_checksum[tkey][vkey].append(record.id)
 
-    abundance_distribution = collections.defaultdict(int)
-
-    for id_list in ids_by_checksum.values():
-        id_count = len(id_list)
-        abundance_distribution[id_count] = abundance_distribution[id_count] + 1
-
-    assert sequence_count == sum(k * v for k, v in abundance_distribution.items())
+    distr_dicts = {k : collections.defaultdict(int) for k in final_distrs}
+    for tkey in final_distrs:
+        for id_list in ids_by_checksum[tkey].values():
+            id_count = len(id_list)
+            distr_dicts[tkey][id_count] = distr_dicts[tkey][id_count] + 1
+        assert sequence_count == sum(k * v for k, v in distr_dicts[tkey].items())
 
     base, _ = os.path.splitext(fasta_path)
     base = os.path.basename(base)
-    abundances[base] = pd.Series(
-        abundance_distribution.values(), index=abundance_distribution.keys()
-    )
+    for tkey in final_distrs:
+        final_distrs[tkey][base] = pd.Series(
+            distr_dicts[tkey].values(), index=distr_dicts[tkey].keys()
+        )
 
 if n_too_small > 0:
     print('    skipped %d files with fewer than %d seqs' % (n_too_small, args.min_seqs))
 if len(init_sizes) > 0:
     print('    downsampled %d samples to %d from initial sizes: %s' % (len(init_sizes), args.max_seqs, ' '.join(str(s) for s in sorted(init_sizes))))
+if n_removed > 0:
+    print('    removed %d seqs with name \'%s\'' % (n_removed, args.naive_name))
 
-to_write = pd.DataFrame(abundances).fillna(0).astype(int)
-ofn = "%s/abundances.csv" % args.outdir
 if not os.path.exists(args.outdir):
     os.makedirs(args.outdir)
-print('  writing to %s'%ofn)
-to_write.to_csv(ofn)
+for fstr in ['abundances', 'hdists']:
+    to_write = pd.DataFrame(final_distrs[fstr]).fillna(0).astype(int)
+    ofn = "%s/%s.csv" % (args.outdir, fstr)
+    print('  writing to %s'%ofn)
+    to_write.to_csv(ofn)
