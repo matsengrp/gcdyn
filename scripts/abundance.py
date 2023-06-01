48b89d982b6bceb17e5509648e8387ecd22b6624
duncan ralph <dkralph@gmail.com>
2023-05-31 18:43:39 -0700

    add lint/black formatting

--- scripts/abundance.py
+++ scripts/abundance.py
@@ -32,28 +32,42 @@ usage: python abundance.py <input fasta files> [--outdir <outdir>]
 
 """
 parser = argparse.ArgumentParser(usage=ustr)
-parser.add_argument('infiles', nargs='+')
-parser.add_argument('--outdir', default=os.getcwd())
-parser.add_argument('--min-seqs', type=int, help='only keep GCs that have this many seqs')
-parser.add_argument('--max-seqs', type=int, help='downsample to this many seqs any GCs that have more than this')
-parser.add_argument('--naive-name', default='naive', help='name of naive sequence, so that it can be skipped')
-parser.add_argument('--random-seed', type=int, default=1, help='random seed for subsampling')
+parser.add_argument("infiles", nargs="+")
+parser.add_argument("--outdir", default=os.getcwd())
+parser.add_argument(
+    "--min-seqs", type=int, help="only keep GCs that have this many seqs"
+)
+parser.add_argument(
+    "--max-seqs",
+    type=int,
+    help="downsample to this many seqs any GCs that have more than this",
+)
+parser.add_argument(
+    "--naive-name",
+    default="naive",
+    help="name of naive sequence, so that it can be skipped",
+)
+parser.add_argument(
+    "--random-seed", type=int, default=1, help="random seed for subsampling"
+)
 args = parser.parse_args()
 
 numpy.random.seed(args.random_seed)
 
 n_too_small, init_sizes, n_removed = 0, [], 0
 abundances = {}
-fdicts = {'hdists' : {}, 'max-abdn-shm' : {}}
+fdicts = {"hdists": {}, "max-abdn-shm": {}}
 
 for fasta_path in args.infiles:
-    records = list(SeqIO.parse(fasta_path, 'fasta'))
+    records = list(SeqIO.parse(fasta_path, "fasta"))
     naive_record = None
     if args.naive_name is not None:
         n_before = len(records)
         new_records = []
         for rcd in records:
-            if rcd.id == args.naive_name:  # doesn't check whether there's more than one naive sequence
+            if (
+                rcd.id == args.naive_name
+            ):  # doesn't check whether there's more than one naive sequence
                 naive_record = rcd
             else:
                 new_records.append(rcd)
@@ -65,13 +79,18 @@ for fasta_path in args.infiles:
         continue
     if args.max_seqs is not None and len(records) > args.max_seqs:
         init_sizes.append(len(records))
-        i_to_keep = numpy.random.choice(list(range(len(records))), size=args.max_seqs)  # i_to_keep is because numpy.random.choice barfs on <records> because each record handles the [] operator (@$$*$~$~!!)
+        i_to_keep = numpy.random.choice(
+            list(range(len(records))), size=args.max_seqs
+        )  # i_to_keep is because numpy.random.choice barfs on <records> because each record handles the [] operator (@$$*$~$~!!)
         records = [records[i] for i in i_to_keep]
 
     # This dictionary will map sequence checksum to the list of squence ids that have that
     # sequence checksum.
     ids_by_checksum = collections.defaultdict(list)
-    hdvals, hd_dict = [], {}  # list of hamming distance to naive for each sequence (hd_dict is just for max_abdn below)
+    hdvals, hd_dict = (
+        [],
+        {},
+    )  # list of hamming distance to naive for each sequence (hd_dict is just for max_abdn below)
     sequence_count = 0
     for record in records:
         sequence_count = sequence_count + 1
@@ -84,7 +103,9 @@ for fasta_path in args.infiles:
         id_count = len(id_list)
         abundance_distribution[id_count] = abundance_distribution[id_count] + 1
     assert sequence_count == sum(k * v for k, v in abundance_distribution.items())
-    for amax, max_abdn_idlists in itertools.groupby(sorted(ids_by_checksum.values(), key=len, reverse=True), key=len):
+    for amax, max_abdn_idlists in itertools.groupby(
+        sorted(ids_by_checksum.values(), key=len, reverse=True), key=len
+    ):
         # print(amax, [len(l) for l in max_abdn_idlists])
         break  # just want the first one (max abundance)
 
@@ -94,26 +115,31 @@ for fasta_path in args.infiles:
         abundance_distribution.values(), index=abundance_distribution.keys()
     )
 
-    fdicts['hdists'][base] = hdvals
-    fdicts['max-abdn-shm'][base] = [int(numpy.median([hd_dict[u] for x in max_abdn_idlists for u in x]))]
+    fdicts["hdists"][base] = hdvals
+    fdicts["max-abdn-shm"][base] = [
+        int(numpy.median([hd_dict[u] for x in max_abdn_idlists for u in x]))
+    ]
 
 if n_too_small > 0:
-    print('    skipped %d files with fewer than %d seqs' % (n_too_small, args.min_seqs))
+    print("    skipped %d files with fewer than %d seqs" % (n_too_small, args.min_seqs))
 if len(init_sizes) > 0:
-    print('    downsampled %d samples to %d from initial sizes: %s' % (len(init_sizes), args.max_seqs, ' '.join(str(s) for s in sorted(init_sizes))))
+    print(
+        "    downsampled %d samples to %d from initial sizes: %s"
+        % (len(init_sizes), args.max_seqs, " ".join(str(s) for s in sorted(init_sizes)))
+    )
 if n_removed > 0:
-    print('    removed %d seqs with name \'%s\'' % (n_removed, args.naive_name))
+    print("    removed %d seqs with name '%s'" % (n_removed, args.naive_name))
 
-print('  writing to %s' % args.outdir)
+print("  writing to %s" % args.outdir)
 if not os.path.exists(args.outdir):
     os.makedirs(args.outdir)
 
 to_write = pd.DataFrame(abundances).fillna(0).astype(int)
-to_write.to_csv('%s/abundances.csv' % args.outdir)
+to_write.to_csv("%s/abundances.csv" % args.outdir)
 
 for fstr, fdct in fdicts.items():
-    with open('%s/%s.csv' % (args.outdir, fstr), 'w') as cfile:
-        writer = csv.DictWriter(cfile, ['fbase', 'vlist'])
+    with open("%s/%s.csv" % (args.outdir, fstr), "w") as cfile:
+        writer = csv.DictWriter(cfile, ["fbase", "vlist"])
         writer.writeheader()
         for fbase, vlist in fdicts[fstr].items():
-            writer.writerow({'fbase' : fbase, 'vlist' : ':'.join(str(v) for v in vlist)})
+            writer.writerow({"fbase": fbase, "vlist": ":".join(str(v) for v in vlist)})
--- scripts/combine-simu-files.py
+++ scripts/combine-simu-files.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+
 # import colored_traceback.always  # need to add this to installation stuff, i'm not sure how to do it atm
 import dill
 
@@ -8,26 +9,43 @@ ustr = """
 Read dill pickled trees and response functions from list of input files, and combine (concatenate) them into one output file
 """
 parser = argparse.ArgumentParser(usage=ustr)
-parser.add_argument('infiles', nargs='+', help='list of files with dill pickled gcdyn simulation')
-parser.add_argument('--outfile', required=True, help='output file name for concatenated simulation info (response functions and trees)')
+parser.add_argument(
+    "infiles", nargs="+", help="list of files with dill pickled gcdyn simulation"
+)
+parser.add_argument(
+    "--outfile",
+    required=True,
+    help="output file name for concatenated simulation info (response functions and trees)",
+)
 args = parser.parse_args()
 
 simfo = []
 for ifn in args.infiles:
-    with open(ifn, 'rb') as dfile:
+    with open(ifn, "rb") as dfile:
         pklfo = dill.load(dfile)
         for tfo in pklfo:
             simfo.append(tfo)
 
-print('  writing %d trees from %d files to %s' % (len(simfo), len(args.infiles), args.outfile))
+print(
+    "  writing %d trees from %d files to %s"
+    % (len(simfo), len(args.infiles), args.outfile)
+)
 if not os.path.exists(os.path.dirname(args.outfile)):
     os.makedirs(os.path.dirname(args.outfile))
-with open(args.outfile, 'wb') as pfile:
+with open(args.outfile, "wb") as pfile:
     dill.dump(simfo, pfile)
 
-with open(args.outfile, 'rb') as pfile:
+with open(args.outfile, "rb") as pfile:
     dfo = dill.load(pfile)
-    treestrs = [str(len(list(tfo['tree'].iter_leaves()))) for tfo in pklfo]
-    birthstrs, deathstrs = [[tfo['%s-response' % k] for tfo in pklfo] for k in ['birth', 'death']]
-    print('    checking info in outfile: %d trees with leaf counts: %s' % (len(pklfo), ' '.join(treestrs)))
-    print('        distinct response fcns:  birth %d  death %d' % (len(set(birthstrs)), len(set(deathstrs))))
+    treestrs = [str(len(list(tfo["tree"].iter_leaves()))) for tfo in pklfo]
+    birthstrs, deathstrs = [
+        [tfo["%s-response" % k] for tfo in pklfo] for k in ["birth", "death"]
+    ]
+    print(
+        "    checking info in outfile: %d trees with leaf counts: %s"
+        % (len(pklfo), " ".join(treestrs))
+    )
+    print(
+        "        distinct response fcns:  birth %d  death %d"
+        % (len(set(birthstrs)), len(set(deathstrs)))
+    )
