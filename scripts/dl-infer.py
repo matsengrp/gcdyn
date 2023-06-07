8dd159919f78b92e0ba2a85a5d4eaeadc701e6c1
duncan ralph <dkralph@gmail.com>
2023-06-07 11:50:36 -0700

    handle new tree n leaf encoding

--- scripts/dl-infer.py
+++ scripts/dl-infer.py
@@ -23,15 +23,16 @@ def make_plot(smpl, df):
     plt.clf()
     sns.set_palette("viridis", 8)
     hord = sorted(set(df["Truth"]))
-    sns.histplot(
-        df,
-        x="Predicted",
-        hue="Truth",
-        hue_order=hord,
-        palette="tab10",
-        bins=30,
-        multiple="stack",
-    ).set(title=smpl)
+    # sns.histplot(df, x="Predicted", hue="Truth", hue_order=hord, palette="tab10", bins=30, multiple="stack", ).set(title=smpl)
+    ax = sns.boxplot(df, x='Truth', y='Predicted', boxprops={'facecolor':'None'})
+    if len(df) < 2000:
+        ax = sns.swarmplot(df, x='Truth', y='Predicted', size=4, alpha=0.6)
+    ax.set(title=smpl)
+    for xv, xvl in zip(ax.get_xticks(), ax.get_xticklabels()):
+        plt.plot([xv - 0.5, xv + 0.5], [float(xvl._text), float(xvl._text)], color='darkred', linestyle='--', linewidth=3, alpha=0.7)
+    # sns.scatterplot(df, x='Truth', y='Predicted')
+    # xvals, yvals = df['Truth'], df['Predicted']
+    # plt.plot([0.95 * min(xvals), 1.05 * max(xvals)], [0.95 * min(yvals), 1.05 * max(yvals)], color='darkred', linestyle='--', linewidth=3, alpha=0.7)
     plt.savefig("%s/%s-hist.svg" % (args.outdir, smpl))
     df.to_csv(csvfn(smpl))
 
@@ -69,6 +70,20 @@ def train_and_test():
         for k in ["birth", "death"]
     }
     samples["trees"] = np.load(args.tree_file)
+    n_leaf_list = ([len(t[0]) for t in samples['trees']])
+    max_leaf_count = max(args.min_n_max_leaves, max(n_leaf_list))  # model complains if this is 70, i'm not sure why but whatever
+    print('    padding encoded trees to max leaf count %d (all leaf counts: %s)' % (max_leaf_count, ' '.join(str(c) for c in set(n_leaf_list))))
+    padded_trees = []
+    for itree, etree in enumerate(samples['trees']):
+        assert len(etree) == 4  # make sure there's 4 rows
+        assert len(set(len(r) for r in etree)) == 1  # and that every row is the same length
+        padded_trees.append(np.pad(etree, ((0, 0), (0, max_leaf_count - len(etree[0])))))
+        # if itree == 0:
+        #     before_len = len(etree[0])
+        #     np.set_printoptions(precision=3, suppress=True, linewidth=99999)
+        #     print('  padded length from %d to %d' % (before_len, len(etree[0])))
+        #     print(etree)
+    samples['trees'] = np.array(padded_trees)
     print("    read %d trees from %s (%d responses from %s)" % (len(samples["trees"]), args.tree_file, len(pklfo), args.response_file))
     print(
         "      first response pair:\n        birth: %s\n        death: %s"
@@ -91,7 +106,7 @@ def train_and_test():
     ]
 
     model = NeuralNetworkModel(
-        None, param_to_predict, network_layers=args.model_size, encoded_trees=smpldict["train"]["trees"]
+        None, param_to_predict, network_layers=args.model_size, encoded_trees=smpldict["train"]["trees"], max_leaf_count=max_leaf_count
     )
     model.fit(epochs=args.epochs)
 
@@ -113,6 +128,7 @@ parser.add_argument("--tree-file", required=True, help="input file with list of 
 parser.add_argument("--response-file", required=True, help="input file with list of response functions in pickle format")
 parser.add_argument("--outdir", required=True, help='output directory')
 parser.add_argument("--epochs", type=int, default=100)
+parser.add_argument("--min-n-max-leaves", type=int, default=200)
 parser.add_argument(
     "--train-frac", type=float, default=0.8, help="train on this fraction of the trees"
 )
