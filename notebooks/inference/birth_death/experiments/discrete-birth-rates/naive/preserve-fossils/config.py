809eefad06b2684f42e12fdb4953a40654f350f9
Thanasi Bakis <abakis@login-i17.local>
2023-06-29 11:32:12 -0700

    More experiments

---
+++ notebooks/inference/birth_death/experiments/discrete-birth-rates/naive/preserve-fossils/config.py
@@ -0,0 +1,97 @@
+import jax.numpy as np
+from scipy.stats import lognorm
+
+from gcdyn import models, mutators, poisson
+from gcdyn.mcmc import Parameter
+
+STATE_SPACE = (2, 4, 6, 8)
+INITIAL_STATE = 2
+PRESENT_TIME = 2
+NUM_TREES = 10
+TREE_SEED = 11
+
+TRUE_PARAMETERS = {
+    "birth_response": poisson.DiscreteResponse(
+        phenotypes=STATE_SPACE, values=(1, 2, 3, 4)
+    ),
+    "death_response": poisson.ConstantResponse(0.5),
+    "mutation_response": poisson.ConstantResponse(0.5),
+    "mutator": mutators.DiscreteMutator(
+        state_space=STATE_SPACE,
+        transition_matrix=np.array(
+            [[0, 1, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1, 1, 0]]
+        )
+        / 3,
+    ),
+    "extant_sampling_probability": 1,
+    "extinct_sampling_probability": 1,
+}
+
+BR_PRIOR_MEAN = 1.5
+BR_PRIOR_SD = 1
+DR_PRIOR_MEAN = -1
+DR_PRIOR_SD = 0.5
+
+BR1_PROPOSAL_SD = 0.5
+BR2_PROPOSAL_SD = 0.5
+DR_PROPOSAL_SD = 0.5
+
+MCMC_PARAMETERS = dict(
+    # Note that lognorm(a, b) in R is lognorm(scale=exp(a), s=b) in scipy
+    birth_rate1=Parameter(
+        prior_log_density=lognorm(scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD).logpdf,
+        prior_generator=lambda n: lognorm(
+            scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD
+        ).rvs(n),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=BR1_PROPOSAL_SD).logpdf(p),
+        proposal_generator=lambda c: lognorm(scale=c, s=BR1_PROPOSAL_SD).rvs(1),
+    ),
+    birth_rate2=Parameter(
+        prior_log_density=lognorm(scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD).logpdf,
+        prior_generator=lambda n: lognorm(
+            scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD
+        ).rvs(n),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=BR2_PROPOSAL_SD).logpdf(p),
+        proposal_generator=lambda c: lognorm(scale=c, s=BR2_PROPOSAL_SD).rvs(1),
+    ),
+    birth_rate3=Parameter(
+        prior_log_density=lognorm(scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD).logpdf,
+        prior_generator=lambda n: lognorm(
+            scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD
+        ).rvs(n),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=BR2_PROPOSAL_SD).logpdf(p),
+        proposal_generator=lambda c: lognorm(scale=c, s=BR2_PROPOSAL_SD).rvs(1),
+    ),
+    birth_rate4=Parameter(
+        prior_log_density=lognorm(scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD).logpdf,
+        prior_generator=lambda n: lognorm(
+            scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD
+        ).rvs(n),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=BR2_PROPOSAL_SD).logpdf(p),
+        proposal_generator=lambda c: lognorm(scale=c, s=BR2_PROPOSAL_SD).rvs(1),
+    ),
+    death_rate=Parameter(
+        prior_log_density=lognorm(scale=np.exp(DR_PRIOR_MEAN), s=DR_PRIOR_SD).logpdf,
+        prior_generator=lambda n: lognorm(
+            scale=np.exp(DR_PRIOR_MEAN), s=DR_PRIOR_SD
+        ).rvs(n),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=DR_PROPOSAL_SD).logpdf(p),
+        proposal_generator=lambda c: lognorm(scale=c, s=DR_PROPOSAL_SD).rvs(1),
+    ),
+)
+
+
+def log_likelihood(
+    birth_rate1, birth_rate2, birth_rate3, birth_rate4, death_rate, trees
+):
+    return models.naive_log_likelihood(
+        trees=trees,
+        birth_response=poisson.DiscreteResponse(
+            phenotypes=STATE_SPACE,
+            values=np.hstack((birth_rate1, birth_rate2, birth_rate3, birth_rate4)),
+        ),
+        death_response=poisson.ConstantResponse(death_rate),
+        mutation_response=TRUE_PARAMETERS["mutation_response"],
+        mutator=TRUE_PARAMETERS["mutator"],
+        extant_sampling_probability=TRUE_PARAMETERS["extant_sampling_probability"],
+    )
---
+++ notebooks/inference/birth_death/experiments/sigmoid-birth-rates/stadler-full/baseline/config.py
@@ -0,0 +1,113 @@
+import jax.numpy as np
+from scipy.stats import gamma, lognorm, norm
+
+from gcdyn import models, mutators, poisson
+from gcdyn.mcmc import Parameter
+
+STATE_SPACE = (2, 4, 6, 8)
+INITIAL_STATE = 2
+PRESENT_TIME = 2
+NUM_TREES = 4
+TREE_SEED = 11
+
+TRUE_PARAMETERS = {
+    "birth_response": poisson.SigmoidResponse(1.0, 5.0, 2.0, 0.5),
+    "death_response": poisson.ConstantResponse(0.5),
+    "mutation_response": poisson.ConstantResponse(0.5),
+    "mutator": mutators.DiscreteMutator(
+        state_space=STATE_SPACE,
+        transition_matrix=np.array(
+            [[0, 1, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1, 1, 0]]
+        )
+        / 3,
+    ),
+    "extant_sampling_probability": 1,
+    "extinct_sampling_probability": 0,
+}
+
+XSCALE_PRIOR_SHAPE = 2
+XSCALE_PRIOR_SCALE = 1
+XSHIFT_PRIOR_MEAN = 5
+XSHIFT_PRIOR_SD = 1
+YSCALE_PRIOR_SHAPE = 2
+YSCALE_PRIOR_SCALE = 1
+YSHIFT_PRIOR_SHAPE = 1
+YSHIFT_PRIOR_SCALE = 1
+DR_PRIOR_MEAN = -1
+DR_PRIOR_SD = 0.5
+
+XSCALE_PROPOSAL_SD = 3
+XSHIFT_PROPOSAL_SD = 2
+YSCALE_PROPOSAL_SD = 1
+YSHIFT_PROPOSAL_SD = 1
+DR_PROPOSAL_SD = 0.5
+
+MCMC_PARAMETERS = dict(
+    xscale=Parameter(
+        prior_log_density=gamma(a=XSCALE_PRIOR_SHAPE, scale=XSCALE_PRIOR_SCALE).logpdf,
+        prior_generator=lambda n: gamma(
+            a=XSCALE_PRIOR_SHAPE, scale=XSCALE_PRIOR_SCALE
+        ).rvs(n),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=XSCALE_PROPOSAL_SD).logpdf(
+            p
+        ),
+        proposal_generator=lambda c: lognorm(scale=c, s=XSCALE_PROPOSAL_SD).rvs(1),
+    ),
+    xshift=Parameter(
+        prior_log_density=norm(loc=XSHIFT_PRIOR_MEAN, scale=XSHIFT_PRIOR_SD).logpdf,
+        prior_generator=lambda n: norm(
+            loc=XSHIFT_PRIOR_MEAN, scale=XSHIFT_PRIOR_SD
+        ).rvs(n),
+        proposal_log_density=lambda p, c: norm(loc=c, scale=XSHIFT_PROPOSAL_SD).logpdf(
+            p
+        ),
+        proposal_generator=lambda c: norm(loc=c, scale=XSHIFT_PROPOSAL_SD).rvs(1),
+    ),
+    yscale=Parameter(
+        prior_log_density=gamma(a=YSCALE_PRIOR_SHAPE, scale=YSCALE_PRIOR_SCALE).logpdf,
+        prior_generator=lambda n: gamma(
+            a=YSCALE_PRIOR_SHAPE, scale=YSCALE_PRIOR_SCALE
+        ).rvs(n),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=YSCALE_PROPOSAL_SD).logpdf(
+            p
+        ),
+        proposal_generator=lambda c: lognorm(scale=c, s=YSCALE_PROPOSAL_SD).rvs(1),
+    ),
+    yshift=Parameter(
+        prior_log_density=gamma(a=YSHIFT_PRIOR_SHAPE, scale=YSHIFT_PRIOR_SCALE).logpdf,
+        prior_generator=lambda n: gamma(
+            a=YSHIFT_PRIOR_SHAPE, scale=YSHIFT_PRIOR_SCALE
+        ).rvs(n),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=YSHIFT_PROPOSAL_SD).logpdf(
+            p
+        ),
+        proposal_generator=lambda c: lognorm(scale=c, s=YSHIFT_PROPOSAL_SD).rvs(1),
+    ),
+    # yshift=Parameter(
+    #     prior_log_density=lambda y: y == TRUE_PARAMETERS["birth_response"].yshift,
+    #     prior_generator=lambda n: np.ones(n) * TRUE_PARAMETERS["birth_response"].yshift,
+    #     proposal_log_density=lambda p, c: p == TRUE_PARAMETERS["birth_response"].yshift,
+    #     proposal_generator=lambda c: TRUE_PARAMETERS["birth_response"].yshift,
+    # ),
+    death_rate=Parameter(
+        prior_log_density=lognorm(scale=np.exp(DR_PRIOR_MEAN), s=DR_PRIOR_SD).logpdf,
+        prior_generator=lambda n: lognorm(
+            scale=np.exp(DR_PRIOR_MEAN), s=DR_PRIOR_SD
+        ).rvs(n),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=DR_PROPOSAL_SD).logpdf(p),
+        proposal_generator=lambda c: lognorm(scale=c, s=DR_PROPOSAL_SD).rvs(1),
+    ),
+)
+
+
+def log_likelihood(death_rate, trees, **birth_params):
+    return models.stadler_full_log_likelihood(
+        trees=trees,
+        birth_response=poisson.SigmoidResponse(**birth_params),
+        death_response=poisson.ConstantResponse(death_rate),
+        mutation_response=TRUE_PARAMETERS["mutation_response"],
+        mutator=TRUE_PARAMETERS["mutator"],
+        extant_sampling_probability=TRUE_PARAMETERS["extant_sampling_probability"],
+        extinct_sampling_probability=TRUE_PARAMETERS["extinct_sampling_probability"],
+        present_time=PRESENT_TIME,
+    )
