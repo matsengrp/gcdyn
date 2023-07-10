d7297d14006428617a5f1e1e774935416bb8461b
Thanasi Bakis <abakis@uci.edu>
2023-07-10 12:22:07 -0700

    Forgot one

--- notebooks/inference/birth_death/experiments/discrete-birth-rates/naive/baseline/mcmc_config.py
+++ notebooks/inference/birth_death/experiments/discrete-birth-rates/naive/baseline/mcmc_config.py
@@ -1,31 +1,9 @@
 import jax.numpy as np
 from scipy.stats import lognorm
+from tree_config import STATE_SPACE, TRUE_PARAMETERS
 
-from gcdyn import models, mutators, poisson
+from gcdyn import models, poisson
 from gcdyn.mcmc import Parameter
-
-STATE_SPACE = (2, 4, 6, 8)
-INITIAL_STATE = 2
-PRESENT_TIME = 2
-NUM_TREES = 10
-TREE_SEED = 10
-
-TRUE_PARAMETERS = {
-    "birth_response": poisson.DiscreteResponse(
-        phenotypes=STATE_SPACE, values=(1, 2, 3, 4)
-    ),
-    "death_response": poisson.ConstantResponse(1.3),
-    "mutation_response": poisson.ConstantResponse(2),
-    "mutator": mutators.DiscreteMutator(
-        state_space=STATE_SPACE,
-        transition_matrix=np.array(
-            [[0, 1, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1, 1, 0]]
-        )
-        / 3,
-    ),
-    "extant_sampling_probability": 1,
-    "extinct_sampling_probability": 0,
-}
 
 BR_PRIOR_MEAN = 1.5
 BR_PRIOR_SD = 1
