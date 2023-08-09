40f8745619a19e33a01ffdbda3507e2a2174cc08
Thanasi Bakis <abakis@uci.edu>
2023-08-09 12:29:55 -0700

    Sigmoid runs

--- notebooks/inference/birth_death/experiments/discrete-birth-rates/baseline/stadler-full/mcmc_config.py
+++ notebooks/inference/birth_death/experiments/discrete-birth-rates/baseline/stadler-full/mcmc_config.py
@@ -7,8 +7,8 @@ from gcdyn.mcmc import Parameter
 
 BR_PRIOR_MEAN = 1.5
 BR_PRIOR_SD = 1
-DR_PRIOR_MEAN = 0  # -1
-DR_PRIOR_SD = 0.3  # 0.5
+DR_PRIOR_MEAN = 0
+DR_PRIOR_SD = 0.3
 
 BR_PROPOSAL_SD = 0.1
 DR_PROPOSAL_SD = 0.2
@@ -38,6 +38,26 @@ MCMC_PARAMETERS = dict(
             1, random_state=rng
         ),
     ),
+    birth_rate3=Parameter(
+        prior_log_density=lognorm(scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD).logpdf,
+        prior_generator=lambda n, rng: lognorm(
+            scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD
+        ).rvs(n, random_state=rng),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=BR_PROPOSAL_SD).logpdf(p),
+        proposal_generator=lambda c, rng: lognorm(scale=c, s=BR_PROPOSAL_SD).rvs(
+            1, random_state=rng
+        ),
+    ),
+    birth_rate4=Parameter(
+        prior_log_density=lognorm(scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD).logpdf,
+        prior_generator=lambda n, rng: lognorm(
+            scale=np.exp(BR_PRIOR_MEAN), s=BR_PRIOR_SD
+        ).rvs(n, random_state=rng),
+        proposal_log_density=lambda p, c: lognorm(scale=c, s=BR_PROPOSAL_SD).logpdf(p),
+        proposal_generator=lambda c, rng: lognorm(scale=c, s=BR_PROPOSAL_SD).rvs(
+            1, random_state=rng
+        ),
+    ),
     death_rate=Parameter(
         prior_log_density=lognorm(scale=np.exp(DR_PRIOR_MEAN), s=DR_PRIOR_SD).logpdf,
         prior_generator=lambda n, rng: lognorm(
@@ -51,7 +71,9 @@ MCMC_PARAMETERS = dict(
 )
 
 
-def log_likelihood(birth_rate1, birth_rate2, death_rate, trees):
+def log_likelihood(
+    birth_rate1, birth_rate2, birth_rate3, birth_rate4, death_rate, trees
+):
     return models.stadler_full_log_likelihood(
         trees=trees,
         birth_response=poisson.DiscreteResponse(
@@ -60,6 +82,8 @@ def log_likelihood(birth_rate1, birth_rate2, death_rate, trees):
                 (
                     birth_rate1,
                     birth_rate2,
+                    birth_rate3,
+                    birth_rate4,
                 )
             ),
         ),
--- notebooks/inference/birth_death/experiments/discrete-birth-rates/baseline/tree_config.py
+++ notebooks/inference/birth_death/experiments/discrete-birth-rates/baseline/tree_config.py
@@ -2,18 +2,23 @@ import jax.numpy as np
 
 from gcdyn import mutators, poisson
 
-STATE_SPACE = (2, 4)
+STATE_SPACE = (2, 4, 6, 8)
 INITIAL_STATE = 2
 PRESENT_TIME = 2
 NUM_TREES = 15
 
 TRUE_PARAMETERS = {
-    "birth_response": poisson.DiscreteResponse(phenotypes=STATE_SPACE, values=(1, 1.5)),
+    "birth_response": poisson.DiscreteResponse(
+        phenotypes=STATE_SPACE, values=(1, 1.5, 2, 2.5)
+    ),
     "death_response": poisson.ConstantResponse(1.3),
     "mutation_response": poisson.ConstantResponse(2),
     "mutator": mutators.DiscreteMutator(
         state_space=STATE_SPACE,
-        transition_matrix=np.array([[0, 1], [1, 0]]),
+        transition_matrix=np.array(
+            [[0, 1, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1, 1, 0]]
+        )
+        / 3,
     ),
     "extant_sampling_probability": 1,
     "extinct_sampling_probability": 0,
